000511a2459d5a5aad7aa183918b2cd3432bb917
devodevM <decodev.contact@gmail.com>
2024-01-19 23:32:33 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yacine on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
